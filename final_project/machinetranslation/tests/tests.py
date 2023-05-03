752deb25c1d3d398deb2aefe8c60ce23ba84f6ff
jaxsol <jackieeeg@gmail.com>
2023-05-02 23:33:07 -0400

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,15 +1,18 @@
-import unittest
+from unittest import TestCase
+from unittest import main
 from translator import french_to_english
 from translator import english_to_french
 
-class TestFrenchToEnglish(unittest.TestCase):
+class TestFrenchToEnglish(TestCase):
     def test1(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
-        self.assertEqual(french_to_english('Bien'), 'Good')
+    def test2(self):
+        self.assertNotEqual(french_to_english('Bien'), 'Bad')
 
-class TestEnglishToFrench(unittest.TestCase):
+class TestEnglishToFrench(TestCase):
     def test1(self):
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
-        self.assertEqual(english_to_french('Good'), 'Bien')
+    def test2(self):
+        self.assertNotEqual(english_to_french('Good'), 'Mauvais')
 
-unittest.main()
+main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -3,34 +3,31 @@ import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
-
 load_dotenv()
-
 apikey = os.environ['apikey']
 url = os.environ['url']
-apikey_lt=IAMAuthenticator("vg7kPENua9nsny6UJTAPBJO1bIEZIbiGmNYaKDYLNBPf")
-VERSION_LT="2018-05-01"
-language_translator=LanguageTranslatorV3(authenticator=apikey_lt, version=VERSION_LT)
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version = '2008-05-01',
+    authenticator = authenticator
+)
+
 language_translator.set_service_url(url)
 
 def english_to_french(english_text):
     '''
-    This function translates English input to French output
+    This function translates english to french
     '''
-    if english_text != "":
-        english_to_french = language_translator.translate(text=english_text, model_id="en-fr").get_result()
-        french_text = english_to_french["translations"][0]["translation"]
-    else:
-        french_text = ""
+    result = language_translator.translate(text = english_text, model_id='en-fr').get_result()
+    french_text =  result['translations'][0]['translation']
     return french_text
 
 def french_to_english(french_text):
     '''
-    This function translates French input to English output
+    This function translates french to english
     '''
-    if french_text != "":
-        french_to_english = language_translator.translate(text=french_text, model_id="fr-en").get_result()
-        english_text = french_to_english["translations"][0]["translation"]
-    else:
-        english_text = ""
+    result =  language_translator.translate(text = french_text, model_id='fr-en').get_result()
+    english_text = result['translations'][0]['translation']
     return english_text
+
